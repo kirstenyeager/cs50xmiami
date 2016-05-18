a629ee417293f30aa6cb37ab83578b97e25c5646
kschipper621 <kirsten@yscpas.com>
2016-05-17 20:42:32 -0400

    finished pset 6

--- psets/6.js
+++ psets/6.js
@@ -14,4 +14,21 @@ Look up the JavaScript string reference to find methods which may be useful!
 
 function mixUp(a, b) {
   // write your solution here...
+
+  var res1 = a.substr(2, 4);
+  var res2 = b.substr(0, 2);
+  var res3 = res2 + res1;
+
+  var res4 = b.substr(2, 4);
+  var res5 = a.substr(0, 2);
+  var res6 = res5 + res4;
+
+  var res7 = res3+ " " +res6;
+
+  return res7;
+
 }
+var str1 = "Hello";
+var str2 = "World!";
+var str3 = mixUp(str1, str2);
+console.log(str3);

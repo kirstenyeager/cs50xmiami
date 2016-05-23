f7a8b8903e80ce9c5cf8b80b759cc5641d02ea7b
kirstenyeager <kirsten@yscpas.com>
2016-05-23 17:57:02 -0400

    Finished pset 7.

--- psets/7.js
+++ psets/7.js
@@ -13,4 +13,22 @@ fixStart('babble'): 'ba**le'
 
 function fixStart(s) {
   // write your solution here...
+// split apart the string into an array
+  var arr = s.split("");
+  var text = "b";
+  var i;
+  // replace each instance of the first character with *
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[0])
+    {
+      arr[i] ="*";
+    }
+      text += arr[i];
+  }
+console.log(text);
+
 }
+
+
+var s = "babble";
+fixStart(s);

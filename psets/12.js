dee794f76eb1d03a01edde1c93f3881f4b4413dc
kirstenyeager <kirsten@yscpas.com>
2016-05-24 15:07:44 -0400

    Finished pset 12.

--- psets/12.js
+++ psets/12.js
@@ -12,3 +12,36 @@ Write a function named calculateSupply that:
 */
 
 // write your solution here...
+
+function  calculateSupply(a, b) {
+  var maximumAge = 90;
+  var totalNeed = (maximumAge - a) * 365 * b;
+  return totalNeed;
+}
+
+function print(x, maximumAge)
+{
+  console.log("You will need " + x + " to last you until the ripe old age of " +maximumAge + ".");
+}
+
+var maximumAge = 90;
+// passing in first set of parameters
+var currentAge = 50;
+var dailyConsumption = 4;
+
+x = calculateSupply(currentAge, dailyConsumption);
+print(x, maximumAge);
+
+// passing in second set of parameters
+ currentAge = 23;
+ dailyConsumption = 10;
+
+ x = calculateSupply(currentAge, dailyConsumption);
+print(x, maximumAge);
+
+// passing in third set of parameters
+currentAge = 2;
+dailyConsumption = 100;
+
+x = calculateSupply(currentAge, dailyConsumption);
+print(x, maximumAge);

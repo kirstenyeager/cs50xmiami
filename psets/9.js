5a9ae9108711e006b7928a63aed6c6bb0dcc75e5
kirstenyeager <kirsten@yscpas.com>
2016-05-24 11:41:34 -0400

    Finished pset 9.

--- psets/9.js
+++ psets/9.js
@@ -15,6 +15,26 @@ For example:
 
 */
 
-function notBad(sentence) {
+function notBad(s) {
   // write your solution here...
+  if (((s.search("not")) !== -1) && ((s.search("bad")) !== -1))
+  {
+    // get index of both "not" and "bad."
+      var l = s.length;
+      var x = s.indexOf("not");
+      var y = s.indexOf("bad");
+      // slice apart the beginnins and end of the sentence.
+      var sent1 = s.slice(0, x);
+      var sent2 = s.slice((y + 3), s.length);
+      // add them back together with "good" in the middle.
+      var newS = sent1 + "good" + sent2;
+  }
+  else
+  {
+     var newS = s;
+  }
+  return newS;
 }
+var sentence = "This restaruant is not altogether so bad, let's eat here again!";
+var goodSentence = notBad(sentence);
+console.log(goodSentence);

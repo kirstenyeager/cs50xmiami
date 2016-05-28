d97ec445f66d60ad4dc0d17d778c789c26068779
kirstenyeager <kirsten@yscpas.com>
2016-05-28 16:44:20 -0400

    Finished pset 17

--- psets/17.js
+++ psets/17.js
@@ -10,3 +10,35 @@ Call that function for a few different scores and log the result to make sure it
 */
 
 // write your solution here...
+function pluralize (num, noun) {
+  if (noun === "sheep")
+  {
+    return num + " " + noun;
+  }
+  if (noun === "goose")
+  {
+    return num + " " + "geese";
+  }
+  if (num === 1)
+  {
+    return num + " " + noun;
+  }
+  else {
+    return num + " " + noun + "s";
+  }
+
+
+}
+
+// var num = 2;
+// var noun = "dog";
+
+var noun = ["dog", "cat", "cow", "penguin", "goose", "sheep"];
+var num = [1, 2, 3, 4, 5, 6];
+var phrase;
+
+for (var i = 0; i < num.length; i++)
+{
+  phrase = pluralize(num[i], noun[i]);
+  console.log (phrase);
+}

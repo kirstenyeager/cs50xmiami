57668e1e86ccde30d4f59cdd1f0c6ddc21809e7e
kirstenyeager <kirsten@yscpas.com>
2016-05-29 00:22:48 -0400

    Finished pset 20

--- psets/20.js
+++ psets/20.js
@@ -9,3 +9,23 @@ Create an array to hold your top choices (colors, presidents, whatever).
 */
 
 // write your solution here...
+var choices = ["blue", "pink", "green", "yellow"];
+
+var suffix;
+
+for (var i = 1; i < 5; i++)
+{
+  if (i === 1){
+    suffix = "st";
+  }
+  else if (i === 2){
+    suffix = "nd";
+  }
+  else if (i === 3) {
+    suffix = "rd";
+  }
+  else {
+    suffix = "th";
+  }
+    console.log("My " + i + suffix +" choice is " + choices[i-1]);
+  }

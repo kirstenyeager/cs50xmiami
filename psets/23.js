537ebed530f49d6b927c07de2582ab21477a8a39
kirstenyeager <kirsten@yscpas.com>
2016-05-29 23:05:33 -0400

    Finished pset 23.

--- psets/23.js
+++ psets/23.js
@@ -11,3 +11,29 @@ It's like IMDB, but much much smaller!
 */
 
 // write your solution here...
+var movieArray = [];
+
+function movie (title, duration, stars)
+{
+  this.title = title;
+  this.duration = duration;
+  this.stars = stars
+}
+
+var big = new movie ("Big", 90, ["cat", "dog"]);
+var titanic = new movie ("Titanic", 210, ["Tom", "Jerry"]);
+var reader = new movie ("The Reader", 100, ["Al", "Hardy"]);
+
+movieArray.push(big);
+movieArray.push(titanic);
+movieArray.push(reader);
+//console.log(big.title + big.stars[0] + big.stars[1]);
+
+for (var i = 0; i < movie.length; i++)
+{
+  console.log(movieArray[i].title + " lasts for " + movieArray[i].duration + " minutes. Stars: \r");
+  for(var j = 0; j< 2; j++)
+  {
+    console.log(movieArray[i].stars[j] + "\r");
+  }
+}
